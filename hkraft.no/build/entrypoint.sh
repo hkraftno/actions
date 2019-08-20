cdbb79e3f37f5ba51dce8361186b933187825dfa
Petter Hareim <petter.hareim@hkraft.no>
2019-08-20 12:51:09 +0200

    feature(hkraft.no): add first iteration of build-script for hkraft.no

---
+++ hkraft.no/build/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+
+sh -c "echo Hello. We would like to build hkraft.no for $ENV. (This is a work in progress)"
+sh -c "npm run build"
---
+++ hkraft.no/deploy/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -l
+
+sh -c "echo Hello. We would like to deploy to $ENV. (This is a work in progress)"
